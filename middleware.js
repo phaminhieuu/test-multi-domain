98b7bdafe8f3c5e64cd4c25819ef60a7dd68f44e
EraaNova <hieupham@makerstudios.gg>
2022-09-23 13:26:21 +0700

    fix middleware

--- middleware.js
+++ middleware.js
@@ -9,8 +9,8 @@ export default function middleware(req) {
   const hostname = req.headers.get("host") || 'app.test-multi-domain.vercel.app'
   const currentHost =
     process.env.NODE_ENV === "production"
-      ? hostname?.replace(`.test-multi-domain.vercel.app`, "")
-      : hostname?.replace(".localhost:3000", "");
+      ? hostname.replace(`.test-multi-domain.vercel.app`, "")
+      : hostname.replace(".localhost:3000", "");
 
   if (currentHost === "app") {
     url.pathname = `/app${url.pathname}`;
